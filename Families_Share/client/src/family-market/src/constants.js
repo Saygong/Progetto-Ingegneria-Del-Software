f71762cb8e1fab554ad52fd8977eac6bdf6f710f
pierluigimarchioro <marchioropierluigi@gmail.com>
2021-12-11 02:45:22 +0100

    Manual merge?

--- Families_Share/client/src/family-market/src/constants.js
+++
@@ -1,9 +0,0 @@
-const CATEGORIES = ["1", "2", "3", "4"];
-const TN_TYPES = ["loan", "donation"];
-const FAMILY_MARKET_BASE_PAGE_URL = "/family-market"
-
-export default {
-    CATEGORIES,
-    TN_TYPES,
-    FAMILY_MARKET_BASE_PAGE_URL
-};
