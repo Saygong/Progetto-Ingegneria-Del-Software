54c208605b5458dccaa47d28bcb8253d540b0106
RuieX <runjiexia28@gmail.com>
2022-01-15 17:38:34 +0100

    resolving guide's path

--- Families_Share/client/src/family-market/src/components/MarketplaceScreen/GuideButton.js
+++ Families_Share/client/src/family-market/src/components/MarketplaceScreen/GuideButton.js
@@ -4,7 +4,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {withRouter} from "react-router-dom";
 import IconItem from "../IconItem";
-import guideFile from "../../guide/dummy_guide.pdf";
+import guideFile from "../../guide/FamilyMarket_Guide.pdf";
 
 /**
  * Class that represents a button used to download a guide for Family market.
